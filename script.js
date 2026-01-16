b5d06d8d2d476219f31eac66da9b034ad94b5f22
zeeddyyyy <87855090+zeeddyyyy@users.noreply.github.com>
2026-01-16 15:36:54 +0100

    Implement getComputerChoice to randomize game moves
    Generate a random integer between 0 and 2 to represent "rock", "paper", or "scissors".

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+let word;
+function getComputerChoice(){
+    return Math.floor(Math.random() * 3);
+}
+
+const num = getComputerChoice();
+
+
+
+
+switch(num){
+    case 0:
+        word = "rock";
+        break;
+    case 1:
+        word = "paper";
+        break;
+
+    case 2:
+        word = "scissors";
+        break;
+
+    default:
+        console.log("Invalid value");
+}
+
+
