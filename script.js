0175aefe9bd3996674ea1b5000b4de63d06b5b72
zeeddyyyy <87855090+zeeddyyyy@users.noreply.github.com>
2026-01-16 16:31:40 +0100

    refactor: encapsulate computer choice logic within function
    Move the switch statement inside getComputerChoice and remove the global variable
     to improve code structure and scope.

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-let word;
+let humanScore = 0;
+let computerScore = 0;
 function getComputerChoice(){
-    return Math.floor(Math.random() * 3);
-}
-
-const num = getComputerChoice();
-
-
-switch(num){
+    let num = Math.floor(Math.random() * 3);
+    switch(num){
     case 0:
-        word = "rock";
-        break;
+        return "rock";
+        
     case 1:
-        word = "paper";
-        break;
-
+        return "paper";
+        
     case 2:
-        word = "scissors";
-        break;
+        return "scissors";
+       
 
     default:
         console.log("Invalid value");
 }
+
+}
+
+
 
 function getHumanChoice(){
     let input = prompt("Choose between Rock, Paper and Scissors");
@@ -30,7 +29,32 @@ function getHumanChoice(){
     return input.toLowerCase();
 }
 
+
+
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice.toLowerCase() === computerChoice){
+        console.log("It's a tie!");
+    } else if(
+        (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors") || 
+        (humanChoice.toLowerCase() === "scissors" && computerChoice ==="paper") ||
+        (humanChoice.toLowerCase() === "paper" && computerChoice ==="rock")
+    ){
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else{
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+
+}
+
+
 const humanChoice = getHumanChoice();
+
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
 
 
 
